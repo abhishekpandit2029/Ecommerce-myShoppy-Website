978f1eb73feeb06d5bdcfe7b3675c176ed45ca13
abhi <abhishekpandit1212@gmail.com>
2023-03-06 11:11:59 +0530

    change horizontalscroll to scrollmenu

--- src/Components/ShopFeed/GirlsField/Feeds/GirlsFusionfeed.js
+++ src/Components/ShopFeed/GirlsField/Feeds/GirlsFusionfeed.js
@@ -2,20 +2,20 @@ import FusionForGirls from "../API/Girlsapi";
 import React from "react";
 import "./Girlsfeed.css";
 import Cards from "../../Source/Cards";
-import HorizontalScroll from "react-scroll-horizontal";
+import ScrollMenu from "react-scroll-horizontal";
 
 function GirlsFusionfeed() {
   const child = { width: `100%`, height: `90%` };
   return (
     <div className="Girlsfeed">
-      <HorizontalScroll reverseScroll={true} animValues={50} style={child}>
+      <ScrollMenu  reverseScroll={true} animValues={50} style={child}>
         {FusionForGirls.map((e) => {
           const { id } = e;
           return <Cards key={id} {...e} />;
         })}
-      </HorizontalScroll>
+      </ScrollMenu >
     </div>
   );
 }
 
-export default GirlsFusionfeed;
+export default GirlsFusionfeed; 
